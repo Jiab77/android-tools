fd355325583bfd057734d826144ab69569c4235d
Jiab77 <jiab77@pm.me>
2023-03-22 16:05:47 +0100

    ⚗️ Added other experimental scripts

---
+++ keep-screen-on.sh
@@ -0,0 +1,44 @@
+#!/usr/bin/env bash
+
+# Basic Android device screen wakeup script
+# Made by Jiab77 / 2023
+#
+# References:
+#
+# Version 0.0.0
+
+# Options
+set +o xtrace
+
+# Config
+AWAKE_INTERVAL=5
+
+# Functions
+function list_devices() {
+    echo -e "\nListing attached devices...\n"
+    adb devices -l | grep -vi "list"
+}
+function get_devices() {
+    echo -e "\nGathering attached devices...\n"
+    if [[ $(adb devices -l | grep -vi "list" | head -n1 | wc -l) -eq 0 ]]; then
+        echo -e "\nError: No attached devices found.\n"
+        exit 1
+    fi
+}
+function wakeup() {
+    echo -e "\nKeeping device screen awake...\n"
+    while true; do date "+%H:%M:%S   %d-%m-%y  -  wake up device"; adb shell input keyevent mouse; sleep $AWAKE_INTERVAL; done
+}
+function init() {
+    get_devices
+    list_devices
+    wakeup
+}
+
+# Usage
+[[ $1 == "-h" || $1 == "--help" ]] && echo -e "\nUsage: $(basename "$0")\n" && exit 1
+
+# Checks
+
+# Main
+init
